0feefa8f827f2f8143aaeef207cfe3d16fa5746b
dima154ak <dima154a@yandex.ru>
2022-10-23 16:28:03 +0500

    Transfer parameters by value

---
+++ References/ViewModel.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "ViewModel.g.h"
+
+namespace winrt::References::implementation
+{
+    struct ViewModel : ViewModelT<ViewModel>
+    {
+        ViewModel() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::References::factory_implementation
+{
+    struct ViewModel : ViewModelT<ViewModel, implementation::ViewModel>
+    {
+    };
+}
