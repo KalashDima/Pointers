11f84fbd15a6131b24910e838b25139ec584d817
dima154ak <dima154a@yandex.ru>
2022-10-23 16:37:13 +0500

    у

--- TransferParameters/main.cpp
+++ TransferParameters/main.cpp
@@ -5,22 +5,8 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void Exchange(int a, int b);
-
 void main()
 {
 	setlocale(LC_ALL, "");
-	int a = 2, b = 3;
-	cout << a << "\t" << b << endl;
-	Exchange(a, b);
-	cout << a << "\t" << b << endl;
-	exchange(a, b);
-	cout << a << "\t" << b << endl;
 }
 
-void Exchange(int a, int b)
-{
-	int buffer = a;
-	a = b;
-	b = buffer;
-}
